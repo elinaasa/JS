61547d705b142d1f0a81ae35e23fba4bc29a3d96
Elina Sankila <sankilaelina@gmail.com>
2022-11-07 22:54:44 +0200

    moooi

---
+++ JSt1.js
@@ -0,0 +1,4 @@
+// console.log('This is printed to the console.');
+// alert('Hi from here too!');
+let name = prompt('Anna nimesi; ');
+document.querySelector('#p1').innerHTML = 'Good morning, ' + name + '!';
