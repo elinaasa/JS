0614733439b297dde394eb144d29463430042ace
Elina Sankila <sankilaelina@gmail.com>
2022-11-11 00:17:58 +0200

    t5 valmis

--- t3.js
+++ t3.js
@@ -1,14 +1,9 @@
 // Write a program that prompts for three integers. The program prints the sum, product and average of the numbers to the HTML document. (3p)
 
-const num1 = prompt('Anna ensimmäinen numero.');
-const num2 = prompt('Anna toinen numero.');
-const num3 = prompt('Anna kolmas numero.');
+const int1 = parseInt(prompt('Anna ensimmäinen numero.'));
+const int2 = parseInt(prompt('Anna toinen numero.'));
+const int3 = parseInt(prompt('Anna kolmas numero.'))
 
-console.log(num1, num2, num3);
-
-const int1 = parseInt(num1);
-const int2 = parseInt(num2);
-const int3 = parseInt(num3);
 
 console.log(int1 + int2 + int3);
 console.log(int1 * int2 * int3);
