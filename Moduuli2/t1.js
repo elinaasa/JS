c4be96535bab700959880220e52a0f1293909615
Elina Sankila <sankilaelina@gmail.com>
2022-11-14 23:14:17 +0200

    t1 valmis

---
+++ Moduuli2/t1.js
@@ -0,0 +1,24 @@
+// Write a program that prompts the user for five numbers and prints them in
+// the reverse order they were entered. Print the result to the console.(2p)
+// Save the numbers to an array, then use for-loop to iterate in reverse order.
+// Do not use array.reverse() function.
+let lista = [];
+let listareverse = []
+for (let i = 0; i < 5; i++) {
+  let numero = parseInt(prompt('Anna numero'));
+  lista.push(numero);
+
+}
+
+for (let i = lista.length - 1; i > -1; i--) {
+  listareverse.push(lista[i]);
+}
+
+
+let list = document.getElementById('htmllista');
+
+for (let item of listareverse) {
+  let li = document.createElement('li');
+  li.innerHTML = item;
+  list.append(li);
+}
