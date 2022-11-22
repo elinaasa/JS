1f753aaca9e6548a1153373d92f845797753e7fc
Omistaja <sankilaelina@gmail.com>
2022-11-22 18:47:53 +0200

    t1

--- Moduulil3/module3-starters-main/t1/1.js
+++ Moduulil3/module3-starters-main/t1/1.js
@@ -1,1 +1,8 @@
-let html = document.getElementById("target").innerHTML;
+let html = document.querySelector('.container');
+let target = document.getElementById('target')
+
+
+
+target.innerHTML = '<li>First item</li>\n' +
+                  '<li>Second item</li>\n' +
+                  '<li>Third item</li>';
