d5bf9420cf9da9d2b71262587950f187d78775d2
Elina Sankila <sankilaelina@gmail.com>
2022-11-24 22:20:12 +0200

    t7

---
+++ Moduulil3/module3-starters-main/t7/7.js
@@ -0,0 +1,10 @@
+let target = document.getElementById('target');
+
+target.addEventListener('mouseenter', (e) => {
+  target.src = 'img/picA.jpg'
+});
+
+target.addEventListener('mouseleave', (e) => {
+  target.src = 'img/picB.jpg'
+});
+
